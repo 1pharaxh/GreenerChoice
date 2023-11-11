049cb650fac53fd12ea703b7151c320c0a36e327
Julia Doan <ngocdath@ualberta.ca>
2023-11-11 13:25:11 -0700

    refactor: change ui colors

--- app/sign-up/[[...sign-up]]/page.tsx
+++ app/sign-up/[[...sign-up]]/page.tsx
@@ -4,7 +4,7 @@ export default function Page() {
   return (
     <div className="container relative h-screen bg-white flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
       <div className="relative hidden h-full flex-col bg-muted p-10 text-white dark:border-r lg:flex">
-        <div className="absolute inset-0 bg-zinc-900" />
+        <div className="absolute inset-0 bg-green-600" />
         <div className="relative z-20 flex items-center text-lg font-medium">
           <svg
             xmlns="http://www.w3.org/2000/svg"
@@ -18,7 +18,7 @@ export default function Page() {
           >
             <path d="M15 6v12a3 3 0 1 0 3-3H6a3 3 0 1 0 3 3V6a3 3 0 1 0-3 3h12a3 3 0 1 0-3-3" />
           </svg>
-          Monsters Inc
+          Greener Choice
         </div>
         <div className="relative z-20 mt-auto">
           <blockquote className="space-y-2">
@@ -35,7 +35,7 @@ export default function Page() {
         <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
           <SignUp
             appearance={{
-              elements: { formButtonPrimary: "bg-zinc-900 hover:bg-zinc-800" },
+              elements: { formButtonPrimary: "bg-green-600 hover:bg-green-500", footerActionLink: "text-green-600 hover-green-700"},
             }}
           />
         </div>
