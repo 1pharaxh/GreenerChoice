ff58a6fd8eb11b7249fc4fc7cd6b3b99bace3c34
Akarshan Mishra <93630550+1pharaxh@users.noreply.github.com>
2023-11-11 15:04:49 -0700

    feat: added first user SignIn Model

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import NewUserQuiz from "@/components/NewUserQuiz";
 import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <UserButton />
+      <NewUserQuiz />
       <a
         href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
         className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
